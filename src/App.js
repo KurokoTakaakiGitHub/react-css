23b29fdd29db4bfe544822dda62d9d59e3405aa8
KurokoTakaakiGitHub <66106869+KurokoTakaakiGitHub@users.noreply.github.com>
2022-03-05 17:04:51 +0900

    Styled JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import { InlineStyle } from "./Components/InlineStyle";
 import { CssModules } from "./Components/CssModules";
+import { StyledJsx } from "./Components/StyledJsx";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
